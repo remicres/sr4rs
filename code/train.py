a4516c31b0607325a62e54f9ed7e68bbafd57952
Remi Cresson <remi.cresson@irstea.fr>
2021-03-10 20:41:14 +0100

    FIX: preview function for image summary

--- code/train.py
+++ code/train.py
@@ -112,7 +112,7 @@ def main(unused_argv):
             tf.identity(gen[1][:, pad:-pad, pad:-pad, :], name="{}{}".format(constants.outputs_prefix, pad))
         if lr_image_for_prev is not None:
             for factor in constants.factors:
-                prev = network.nice_preview(gen[factor], refs=[lr_image_for_prev])
+                prev = network.nice_preview(gen[factor])
                 tf.compat.v1.summary.image("preview_factor{}".format(factor), prev, collections=[constants.epoch_key])
 
         # discriminator
